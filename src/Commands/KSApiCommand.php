f81a4fbe8397618e69de6f56d436c120db08dc42
Niklas Palmqvist <niklas@decisign.se>
2023-10-10 16:56:29 +0200

    Try to build env

--- src/Commands/KSApiCommand.php
+++ src/Commands/KSApiCommand.php
@@ -2,18 +2,21 @@
 
 namespace SMLNordic\KSApi\Commands;
 
+use Dotenv\Dotenv;
+use Dotenv\Repository\Adapter\EnvConstAdapter;
+use Dotenv\Repository\Adapter\PutenvAdapter;
+use Dotenv\Repository\RepositoryBuilder;
 use Illuminate\Console\Command;
-use Symfony\Component\Process\Process;
-use Symfony\Component\Process\Exception\ProcessFailedException;
 
 class KSApiCommand extends Command
 {
+
     public $signature = 'ks-api-init';
 
     public $description = 'Install and publish config';
 
     private $envs = [
-        'KS_API_TOKEN' => '',
+        'KS_API_TOKEN'    => '',
         'KS_API_BASE_URL' => '',
     ];
 
@@ -21,20 +24,36 @@ class KSApiCommand extends Command
     {
 
         if ($this->confirm('This will reset all your KSAPI env vars that has already been set. Continue?')) {
-            foreach($this->envs as $key => $value) {
-                $process = Process::fromShellCommandline('php artisan env:set '.$key.' "'.$value.'"');
-                $process->run();
-                // executes after the command finishes
-                if ( ! $process->isSuccessful()) {
-                    throw new ProcessFailedException($process);
-                }
-
-                echo $process->getOutput();
-            }
+            self::updateEnvFile($this->envs);
             $this->comment('All done');
         } else {
             $this->comment('Aborting...');
         }
+
+    }
+
+    public function updateEnvFile(array $data)
+    {
+
+        // Load the .env file into a repository
+        $repository = RepositoryBuilder::createWithNoAdapters()
+            ->addAdapter(EnvConstAdapter::class)
+            ->addWriter(PutenvAdapter::class)
+            ->immutable()
+            ->make();
+
+        // Loop through the data and update the values
+        foreach ($data as $key => $value) {
+            $repository->set($key, $value);
+        }
+
+        // Save the changes back to the .env file
+        $factory = new DotenvFactory($repository, true);
+        $dotenv = Dotenv::create($repository, $factory);
+        $dotenv->safeLoad();
+        $dotenv->toEnv();
+
+        return true;
 
     }
 }
