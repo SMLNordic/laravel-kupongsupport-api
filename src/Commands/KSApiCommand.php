386f5572703ed85d73c84c9e71333640392fae0d
Niklas Palmqvist <niklas@decisign.se>
2023-10-10 16:11:21 +0200

    Add init script

--- src/Commands/KSApiCommand.php
+++ src/Commands/KSApiCommand.php
@@ -3,15 +3,38 @@
 namespace SMLNordic\KSApi\Commands;
 
 use Illuminate\Console\Command;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 
 class KSApiCommand extends Command
 {
-    public $signature = 'kupongsupport-api';
+    public $signature = 'ks-api-init';
 
-    public $description = 'My command';
+    public $description = 'Install and publish config';
+
+    private $envs = [
+        'KS_API_TOKEN' => '',
+        'KS_API_BASE_URL' => '',
+    ];
 
     public function handle()
     {
-        $this->comment('All done');
+
+        if ($this->confirm('This will reset all your KSAPI env vars that has already been set. Continue?')) {
+            foreach($this->envs as $key => $value) {
+                $process = Process::fromShellCommandline('php artisan env:set '.$key.' "'.$value.'"');
+                $process->run();
+                // executes after the command finishes
+                if ( ! $process->isSuccessful()) {
+                    throw new ProcessFailedException($process);
+                }
+
+                echo $process->getOutput();
+            }
+            $this->comment('All done');
+        } else {
+            $this->comment('Aborting...');
+        }
+
     }
 }
